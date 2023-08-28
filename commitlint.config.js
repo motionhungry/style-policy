675cc643538f687b65686b5b315f584a022f7b14
Kyle Getrost <1052366+kyleget@users.noreply.github.com>
2023-08-28 16:14:51 +0700

    fix(deps): fix conventional commit types and scopes (#4)
    * fix(config): extending commit lint message types
    
    * fix: make commitlint scopes optional
    
    * fix: fix Github Actions config
    
    * fix(config): fix commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,29 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'release',
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['config', 'deps', 'eslint', 'prettier', 'typescript'],
+    ],
+    'scope-empty': [1, 'never'],
+  },
+};
