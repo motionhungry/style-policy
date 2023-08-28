ec8d43d56d826afa3f7660aba9959b1394491d63
Kyle Getrost <kyle@motionhungry.com>
2023-08-28 16:03:50 +0700

    fix(config): extending commit lint message types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,29 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'release',
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['deps', 'eslint', 'prettier', 'typescript', 'golang'],
+    ],
+    'scope-empty': [1, 'never'],
+  },
+};
